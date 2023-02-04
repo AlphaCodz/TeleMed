baf3454e4552649b4c7d23a6542a41865885b95c
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-02-04 22:31:21 +0100

    URL linking now working

--- WebApp/MeHealth/urls.py
+++ WebApp/MeHealth/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path
 from .views import index
 from . import views
 
+app_name = "med"
+
 urlpatterns = [
     path("home/", views.index, name="index"),
     re_path(r"^doctors/$",views.doctors, name="doctors"),
